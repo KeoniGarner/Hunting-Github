1f6a76b9366fa2041a1f691e493c927958b63064
Keoni Garner <keoni_garner@yahoo.com>
2018-05-31 10:55:11 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.get("https://api.github.com/users/KeoniGarner", displayName)
+    // Notice that displayName is a function that takes in 1 parameter (this is the data that comes back from the API)copy
+    function displayName(data) {
+        $('form').append('<p>' + data.name + '</p>');
+    }
+});    
